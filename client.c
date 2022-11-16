12c455b579e045ea075f402084939840b6f26454
cryptogus <wnelem1234@naver.com>
2022-11-16 13:13:39 +0900

    begin2

--- client.c
+++ client.c
@@ -1,3 +1,6 @@
+//gcc client.c -o client
+//./client [ip주소] [포트번호]
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,23 +9,60 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
-int main(int argc, char **argv){
 
-    int sock;
-    struct sockaddr_in serv_addr;
-    char message[30];
-    int str_len;
+#define BUFSIZE 512
 
-    sock = socket(PF_INET, SOCK_STREAM, 0);
 
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    serv_addr.sin_port = htons(atoi(argv[2]));
-    connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
-    str_len = read(sock, message, sizeof(message));
-    message[str_len] = 0;
-    printf("Message from server : %s \n", message);
+int main(int argc, char *argv[]){
+    
+    int retval;
 
-    return 0;
+	// 소켓 생성
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	// connect()
+	struct sockaddr_in serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	inet_pton(AF_INET, argv[1], &serveraddr.sin_addr);
+	serveraddr.sin_port = htons(atoi(argv[2]));
+	retval = connect(sock, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+
+	// 데이터 통신에 사용할 변수
+	char buf[BUFSIZE + 1];
+	int len;
+
+	// 서버와 데이터 통신
+	while (1) {
+		// 데이터 입력
+		printf("\n[보낼 데이터] ");
+		if (fgets(buf, BUFSIZE + 1, stdin) == NULL)
+			break;
+
+		// '\n' 문자 제거
+		len = (int)strlen(buf);
+		if (buf[len - 1] == '\n')
+			buf[len - 1] = '\0';
+		if (strlen(buf) == 0)
+			break;
+
+		// 데이터 보내기
+		retval = send(sock, buf, (int)strlen(buf), 0);
+		
+		printf("[TCP 클라이언트] %d바이트를 보냈습니다.\n", retval);
+
+		// 데이터 받기
+		retval = recv(sock, buf, retval, MSG_WAITALL);
+		if (retval == 0)
+			break;
+
+		// 받은 데이터 출력
+		buf[retval] = '\0';
+		printf("[TCP 클라이언트] %d바이트를 받았습니다.\n", retval);
+		printf("[받은 데이터] %s\n", buf);
+	}
+
+	// 소켓 닫기
+	close(sock);
+	return 0;
 }
--- server.c
+++ server.c
@@ -1,3 +1,6 @@
+//gcc server.c -o server
+//./server [포트번호]
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,29 +9,66 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
-int main(int argc, char **argv){
+#define BUFSIZE 512
 
-    int server_sock;
-    int client_sock;
-    int client_addr_size;
-    char message[] = "Hello World\n";
+int main(int argc, char *argv[]){
 
-    struct sockaddr_in serv_addr;
-    
-    server_sock = socket(PF_INET, SOCK_STREAM, 0);
+    int retval;
 
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(atoi(argv[1]));
-    
-    bind(server_sock,(struct sockaddr*) &serv_addr, sizeof(serv_addr) );
-    listen(server_sock,5);
-    
-    client_addr_size = sizeof(client_addr_size);
-    client_sock = accept(server_sock, (struct sockaddr*) &client_sock, &client_addr_size);
-    write(client_sock, message, sizeof(message));
-    close(client_sock);
+	// 소켓 생성
+	int listen_sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    return 0;
+	// bind()
+	struct sockaddr_in serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(atoi(argv[1]));
+	retval = bind(listen_sock, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+
+	// listen()
+	retval = listen(listen_sock, SOMAXCONN);
+
+	// 데이터 통신에 사용할 변수
+	int client_sock;
+	struct sockaddr_in clientaddr;
+	socklen_t addrlen;
+	char buf[BUFSIZE + 1];
+
+	while (1) {
+		// accept()
+		addrlen = sizeof(clientaddr);
+		client_sock = accept(listen_sock, (struct sockaddr *)&clientaddr, &addrlen);
+
+		// 접속한 클라이언트 정보 출력
+		char addr[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+		printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
+			addr, ntohs(clientaddr.sin_port));
+
+		// 클라이언트와 데이터 통신
+		while (1) {
+			// 데이터 받기
+			retval = recv(client_sock, buf, BUFSIZE, 0);
+			if (retval == 0)
+				break;
+
+			// 받은 데이터 출력
+			buf[retval] = '\0';
+			printf("[TCP/%s:%d] %s\n", addr, ntohs(clientaddr.sin_port), buf);
+
+			// 데이터 보내기
+			retval = send(client_sock, buf, retval, 0);
+
+		}
+
+		// 소켓 닫기
+		close(client_sock);
+		printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n",
+			addr, ntohs(clientaddr.sin_port));
+	}
+
+	// 소켓 닫기
+	close(listen_sock);
+	return 0;
 }
